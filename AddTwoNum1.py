a07a4f6393ff6be37532634f7184d8943bc6c45d
Charsh177 <harshchougule177@gmail.com>
2022-06-14 15:21:05 +0530

    ADDED: Program to addition of two numbers using format

---
+++ AddTwoNum1.py
@@ -0,0 +1,7 @@
+"""Adding two numbers method 1"""
+
+number1 = input("Enter first Number : ")
+number2 = input("Enter second Number : ")
+
+sum = number1 + number2
+print("The sum of {0} and {1} is {2}".format(number1, number2, sum))
