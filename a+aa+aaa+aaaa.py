a994ed3b862af1276c46eb7fd2d502c746c9067c
Charsh177 <harshchougule177@gmail.com>
2022-06-14 15:06:04 +0530

    ADDED: Program computes the value of a+aa+aaa+aaaa

---
+++ a+aa+aaa+aaaa.py
@@ -0,0 +1,10 @@
+"""Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a """
+
+# input any type
+value = input("enter a value : ")
+n1 = 1 * value
+n2 = 2 * value
+n3 = 3 * value
+n4 = 4 * value
+
+print(n1 + n2 + n3 + n4)
