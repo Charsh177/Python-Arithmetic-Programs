e12b48a4aa698bdc6a64009c21b869dcec27484b
Charsh177 <harshchougule177@gmail.com>
2022-06-14 15:38:46 +0530

    ADDED: Program addition of two numbers using add function

---
+++ AddTwoNumDefFunction.py
@@ -0,0 +1,9 @@
+"""Adding two number using function"""
+
+def add(number1, number2):
+    sum = number1 + number2
+    return sum
+
+number1 = 12
+number2 = 20
+print("The sum of two number is ", add(number1, number2))
